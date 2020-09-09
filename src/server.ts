8d5e93641a6216e0376dcb080620d10fd0a0ce72
Fernando Henrique <liquuid@gmail.com>
2020-09-09 11:57:48 -0300

    defining routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 import express, { request, response } from 'express';
+import routes from './routes'
 
 const app = express();
 app.use(express.json());
-app.post('/users', (request, response) => {
-    const { name, email} = request.body;
-    
-    const user = {
-        name,
-        email,
-    };
-    return response.json(user);
-})
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('Server started on port: 3333 🐈')
