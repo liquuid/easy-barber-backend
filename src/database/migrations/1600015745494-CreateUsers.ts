5e47b17ea94df51c977f8803ad911e057234c713
Fernando Henrique <liquuid@gmail.com>
2020-09-13 17:04:47 -0300

    model users

---
+++ src/database/migrations/1600015745494-CreateUsers.ts
@@ -0,0 +1,53 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+
+export default class CreateUsers1600015745494 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary:  true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
