c14ca5c142ab4ae96e8d910b2ce4fbec8b52b610
Fernando Henrique <liquuid@gmail.com>
2020-09-15 11:39:17 -0300

    static route to serve static files

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import 'reflect-metadata';
 import express, { request, response } from 'express';
 import routes from './routes'
 import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
-
+app.use('/files', express.static(uploadConfig.directory));
 app.listen(3333, () => {
     console.log('Server started on port: 3333 🐈')
 })
