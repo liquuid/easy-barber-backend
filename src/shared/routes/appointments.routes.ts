ca7292cc18e8fd0e7e965707a69fa2b65c9d125e
Fernando Henrique <liquuid@gmail.com>
2020-11-05 01:34:25 -0300

    refactoring modules

--- src/shared/routes/appointments.routes.ts
+++ src/shared/routes/appointments.routes.ts
@@ -3,7 +3,7 @@ import { parseISO } from 'date-fns';
 import { getCustomRepository} from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import CreateAppointmentService from '../services/CreateAppointmentService';
+import CreateAppointmentService from '../modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
