2929909e9f54aa9f4466f6882695b0c1d0b2fba0
Fernando Henrique <liquuid@gmail.com>
2020-12-08 23:23:53 -0300

    refactor in modules

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository} from 'typeorm';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,22 +7,24 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 
-appointmentsRouter.get('/', ensureAuthenticated, async (request, response) => {
+/*appointmentsRouter.get('/', ensureAuthenticated, async (request, response) => {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointments = await appointmentsRepository.find();
 
     return response.json(appointments);
 });
+*/
 
 appointmentsRouter.post('/', async (request, response) => {
 
         const { provider_id, date } = request.body;
         const parsedDate = parseISO(date);
-        const createAppointment = new CreateAppointmentService();
+        const createAppointment = new CreateAppointmentService(appointmentsRepository);
         const appointment = await createAppointment.execute({date: parsedDate, provider_id});
         return response.json(appointment);
 
