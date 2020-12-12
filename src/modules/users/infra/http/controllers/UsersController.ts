a898751f6d6bb1be78e7fcb668eecd4aa62deb27
Fernando Henrique <liquuid@gmail.com>
2020-12-12 01:06:24 -0300

    cleanup

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -2,12 +2,10 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateAvatarUserService from '@modules/users/services/UpdateUserAvatarService';
-import AuthenticateUserService from '@modules/users/services/AthenticateUserService';
-
 
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
+
     const { name, email, password } = request.body;
 
     const createUser = container.resolve(CreateUserService);
@@ -19,15 +17,6 @@ export default class UsersController {
     });
     delete user.password;
 
-    return response.json(user);
-  }
-  public async create(request: Request, response: Response): Promise<Response> {
-    const updateUserAvatar =  container.resolve(UpdateAvatarUserService);
-    const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-    });
-    delete user.password
     return response.json(user);
   }
 }
